05b3647f9f2981119cc94b800dddc5a5c12ba2fa
Kenzie <admin@moderocky.com>
2022-11-01 10:40:14 +0000

    Add dataset catcher to self.

--- src/main/java/mx/kenzie/eris/api/entity/Self.java
+++ src/main/java/mx/kenzie/eris/api/entity/Self.java
@@ -2,13 +2,17 @@ package mx.kenzie.eris.api.entity;
 
 import mx.kenzie.eris.data.Payload;
 
+import java.util.Map;
+
 public class Self extends User {
     
     public String session_type, session_id;
     
-    public Payload[] relationships, presences, guild_join_requests; // todo
+    public Payload[] relationships, presences, guild_join_requests; // todo where do these actually come from?
     public Channel[] private_channels;
     public Guild[] guilds;
     public String[] geo_ordered_rtc_regions;
+    
+    public transient Map<String, Object> __data;
     
 }
