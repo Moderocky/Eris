b0968ee43ca3e463ddf1bb544955d9a5334704d2
Moderocky <admin@moderocky.com>
2024-08-24 14:28:27 +0100

    Add appender helper method.

--- src/main/java/mx/kenzie/eris/api/entity/message/UnsentMessage.java
+++ src/main/java/mx/kenzie/eris/api/entity/message/UnsentMessage.java
@@ -1,19 +1,23 @@
 package mx.kenzie.eris.api.entity.message;
 
-import mx.kenzie.grammar.Optional;
 import mx.kenzie.eris.api.entity.Message;
+import mx.kenzie.grammar.Optional;
 
 public abstract class UnsentMessage extends InteractionMessage {
+
     public @Optional String payload_json;
     public @Optional Message.Reference message_reference;
     public @Optional String[] sticker_ids;
-
-    // files[n]
 
     protected transient boolean sent0;
 
     protected UnsentMessage() {
         assert this instanceof Message : "This class should not be instantiated directly.";
+    }
+
+    public void append(String text) {
+        if (content == null) content = text;
+        else content += text;
     }
 
 }
